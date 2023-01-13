a82ed9dba82d9f48d575f7064e24a705b64f5d1a
kyo705 <mission0705@naver.com>
2023-01-13 17:00:54 +0900

    [INIT] 프로젝트 생성

---
+++ src/main/java/com/lolsearcher/persistence/mostchamp/MostChampApplication.java
@@ -0,0 +1,13 @@
+package com.lolsearcher.persistence.mostchamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostChampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostChampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolsearcher/persistence/mostchamp/MostChampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolsearcher.persistence.mostchamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostChampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
